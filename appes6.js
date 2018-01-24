5dab2c47e3577ce4fc2afb8bc5cb3cf06662c521
4Chazz <chastitylawson1@gmail.com>
2018-01-24 14:54:02 -0600

    Add local storage

--- appes6.js
+++ appes6.js
@@ -55,6 +55,55 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks(){
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function(book) {
+     const ui = new UI;
+
+     // Add book to UI
+     ui.addBookToList(book);
+    })
+
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// Dom Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
   // Get form vales
@@ -76,6 +125,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to Storage
+    Store.addBook(book);
+
     //Show success
     ui.showAlert('Book Added!', 'success');
 
@@ -95,6 +147,8 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
   ui.deleteBook(e.target);
 
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   //Show Message
   ui.showAlert('Book Removed!', 'success');
 
